97b1f8104ead27874b43e8d8dfed14184a2706ed
Awesome Vic <mr526@vsutan-mac01.local>
2014-09-08 21:27:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SanSquare::Application.config.secret_key_base = '099d28390449b64f4b4c7baf3eef32b90837f734458ee0621476c5930c4152c858296217e6923a6d21e6abf5fb726be6a3b37d48ac1e8530f0cd056834ee9b57'
