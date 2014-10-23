35c4a2f88cc9c0937471ec647de10dcf773a26cc
Annika Lee <Annika@Annikas-MacBook-Air.local>
2014-10-22 22:07:58 -0700

    added images and carousel to the front pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SanSquare::Application.routes.draw do
+  root to: 'static_pages#home'
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
